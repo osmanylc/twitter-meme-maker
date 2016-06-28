64fc414e2a4a31ea616f40631115c13b43ec918a
Osmany Corteguera <osmanyc@mit.edu>
2016-06-27 20:13:51 -0400

    Refactored variables and modularized code.

--- static/make_meme.js
+++ static/make_meme.js
@@ -1,130 +1,130 @@
-var text_css = {"font-family" : "Helvetica Neue,Helvetica,Arial,sans-serif",
-				"font-size" : "26px",
-			    "line-height" : "32px",
-			    "font-weight" : "300", 
-			    "letter-spacing" : ".01em", 
-				"word-wrap" : "break-word", 
-				"color" : "#292f33", 
-				"width" : "inherit"
-			};
+var textCSS = {"font-family" : "Helvetica Neue,Helvetica,Arial,sans-serif",
+                "font-size" : "26px",
+                "line-height" : "32px",
+                "font-weight" : "300", 
+                "letter-spacing" : ".01em", 
+                "word-wrap" : "break-word", 
+                "color" : "#292f33", 
+                "width" : "inherit"
+            };
 
-var image_css = {"border-radius": "5px", 
-			    "margin-top": "10px",
-			    "position": "relative", 
-			    "display" : "block", 
-			    "overflow": "hidden", 
-			    "vertical-align": "top"
-			};
+var imageCSS = {"border-radius": "5px", 
+                "margin-top": "10px",
+                "position": "relative", 
+                "display" : "block", 
+                "overflow": "hidden", 
+                "vertical-align": "top"
+            };
 
-var meme_container_css = {"background-color" : "#fff",
-						"width" : "500px", 
-						"padding" : "10px", 
-					    "border-bottom" : "0", 
-					    "border-top-left-radius" : "5px", 
-					    "border-top-right-radius" : "5px",
-					    "overflow": "hidden", 
-					    "display": "inline-block"
-					};
+var memeContainerCSS = {"background-color" : "#fff",
+                        "width" : "500px", 
+                        "padding" : "10px", 
+                        "border-bottom" : "0", 
+                        "border-top-left-radius" : "5px", 
+                        "border-top-right-radius" : "5px",
+                        "overflow": "hidden", 
+                        "display": "inline-block"
+                    };
 
-function make_meme_html(text, image_url) {
-	//define elements
-	var div_container = document.createElement("div");
-	var text_div = document.createElement("div");
-	var image_container = document.createElement("img");
+//define elements
+var memeContainer = document.createElement("div");
+var textContainer = document.createElement("div");
+var imageContainer = document.createElement("img");
 
-	//removes automatic padding and margin in the body element
-	var style = document.createElement("style");
-	style.innerHTML = "body {padding:0; margin:0}";
+//removes automatic padding and margin in the body element
+var style = document.createElement("style");
+style.innerHTML = "body {padding:0; margin:0}";
 
-	//build element hierarchy
-	div_container.appendChild(style);
-	$(text_div).append(text);
-	$(image_container).attr("src", image_url);
-	$(div_container).append(text_div);
-	$(div_container).append(image_container);
+//build element hierarchy
+memeContainer.appendChild(style);
+memeContainer.appendChild(textContainer);
+memeContainer.appendChild(imageContainer);
 
-	//make sure image doesn't go over the height and width limits.
-	//also preserves image proportions.
-	if (image_container.naturalHeight > image_container.naturalWidth) {
-		$(image_container).css({"height" : "450px", "width" : "auto", 
-			"margin-left" : "auto", "margin-right" : "auto", "padding-right" : "20px"});
-	} else {
-		$(image_container).css({"width" : "500px", "height" : "auto"});
-	}
-
-
-	//set elements' css
-	$(div_container).css(meme_container_css);
-	$(text_div).css(text_css);
-	$(image_container).css(image_css);
-
-	return $(div_container)[0];
-}
+$(memeContainer).css(memeContainerCSS);
+$(textContainer).css(textCSS);
+$(imageContainer).css(imageCSS);
 
 /* Runs when the page finishes loading */
 $(document).ready(function() {
-	/* Fires when the "Create" button is clicked */
-	$('#create_button').click(function(e) {
-		var textarea = document.getElementById('meme_text');
-		var file_input = document.getElementById('meme_image');
-
-		//checks if both an image and text are present
-		if(file_input.files[0] && textarea.value) {
-			var meme_image = file_input.files[0];
-			var meme_text = textarea.value;
-
-			var reader = new FileReader();
-			reader.onload = function() {
-				var meme_image_url = reader.result;
-				var element = make_meme_html(meme_text, meme_image_url);
-
-				//used to get the size of the canvas
-				element.style.visibility = "hidden";
-				document.body.appendChild(element);
-				var bounding_rect = element.getBoundingClientRect();
-				document.body.removeChild(element);
-				element.style.visibility = "";
-
-				var canvas_width = parseInt(bounding_rect.width, 10);
-				var canvas_height = parseInt(bounding_rect.height, 10);
-
-				//make canvas element
-				var canvas = document.createElement("canvas");
-				canvas.width = canvas_width;
-				canvas.height = canvas_height;
-				var ctx = canvas.getContext('2d');
-
-				//paint background white to prevent png transparency
-				ctx.fillStyle = 'white';
-				ctx.fillRect(0, 0, canvas_width, canvas_height);
-
-				//call rasterizeHTML with html we created
-				var html = element.outerHTML;
-				rasterizeHTML.drawHTML(html, canvas).then(function success(renderResult) {
-					//convert canvas to a png image and attach it to the page
-					var final_src = canvas.toDataURL();
-
-					var final_img = new Image();
-					final_img.src = final_src;
-
-					var a = document.createElement('a');
-					a.innerHTML = 'Download';
-					a.href = final_src;
-					a.download = "meme_" + Date.now();
-
-					document.body.appendChild(final_img);
-					document.body.appendChild(a);
-
-				}, function error(e) {
-					console.log('there was an error');
-				});
-			};
-
-			reader.readAsDataURL(meme_image);
-		}
-		//If image or text missing, alert the user
-		else {
-			alert("Missing text or picture");
-		}
-	});
+    // Fires when the "Create" button is clicked
+    $('#create_button').click(onClickCreate);
 });
+
+function onClickCreate() {
+    var memeTextArea = document.getElementById('meme_text');
+    var memeImageInput = document.getElementById('meme_image');
+
+    //checks if both an image and text are present
+    if(memeImageInput.files[0] && memeTextArea.value) {
+        var memeImage = memeImageInput.files[0];
+        var memeText = memeTextArea.value;
+
+        var reader = new FileReader();
+        reader.onload = createMemeAfterImageIsURI(memeText, reader);
+        reader.readAsDataURL(memeImage);
+    }
+    //If image or text missing, alert the user
+    else {
+        alert("Missing text or picture");
+    }
+}
+
+function setMemeHTML(memeText, memeImageDataURI) {
+    textContainer.innerHTML = memeText;
+    imageContainer.src = memeImageDataURI;
+
+    //make sure image doesn't go over the height and width limits.
+    //also preserves image proportions.
+    if (imageContainer.naturalHeight > imageContainer.naturalWidth) {
+        $(imageContainer).css({"height" : "450px", "width" : "auto", 
+            "margin-left" : "auto", "margin-right" : "auto", "padding-right" : "20px"});
+    } else {
+        $(imageContainer).css({"width" : "500px", "height" : "auto"});
+    }
+}
+
+function createMemeAfterImageIsURI(memeText, reader) {
+    return function () {
+        var memeImageDataURI = reader.result;
+        setMemeHTML(memeText, memeImageDataURI);
+
+        //used to get the size of the canvas
+        memeContainer.style.visibility = "hidden";
+        document.body.appendChild(memeContainer);
+        var boundingRect = memeContainer.getBoundingClientRect();
+        document.body.removeChild(memeContainer);
+        memeContainer.style.visibility = "";
+
+        var canvasWidth = parseInt(boundingRect.width, 10);
+        var canvasHeight = parseInt(boundingRect.height, 10);
+
+        //make canvas element
+        var canvas = document.createElement("canvas");
+        canvas.width = canvasWidth;
+        canvas.height = canvasHeight;
+        var ctx = canvas.getContext('2d');
+
+        //paint background white to prevent png transparency
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+        //call rasterizeHTML with html we created
+        var html = memeContainer.outerHTML;
+        rasterizeHTML.drawHTML(html, canvas).then(function success(renderResult) {
+            //convert canvas to a png image and attach it to the page
+            var finalSrc = canvas.toDataURL();
+            var finalImg = new Image();
+            finalImg.src = finalSrc;
+
+            var a = document.createElement('a');
+            a.innerHTML = 'Download';
+            a.href = finalSrc;
+            a.download = "meme_" + Date.now();
+
+            document.body.appendChild(finalImg);
+            document.body.appendChild(a);
+        }, function error(e) {
+                console.log('there was an error');
+        });
+    };
+}
