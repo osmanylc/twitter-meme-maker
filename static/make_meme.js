368928abe90ea0e73ec572a8b2301b52090fd74e
Osmany Corteguera <osmanyc@mit.edu>
2016-07-23 01:11:43 -0400

    Made code more readable and organized.

--- static/make_meme.js
+++ static/make_meme.js
@@ -1,129 +1,67 @@
-var memePadding = 10;
-
-var textCSS = {"font-family" : "Helvetica Neue,Helvetica,Arial,sans-serif",
-                "font-size" : "26px",
-                "line-height" : "32px",
-                "font-weight" : "300", 
-                "letter-spacing" : ".01em", 
-                "word-wrap" : "break-word", 
-                "color" : "#292f33", 
-                "width" : "inherit"
-            };
-
-var imageCSS = {"border-radius": "5px", 
-                "margin-top": "10px",
-                "position": "relative", 
-                "display" : "block", 
-                "overflow": "hidden", 
-                "vertical-align": "top"
-            };
-
-var memeContainerCSS = {"background-color" : "#fff",
-                        "width" : "500px", 
-                        "padding" : memePadding + "px", 
-                        "border-bottom" : "0", 
-                        "border-top-left-radius" : "5px", 
-                        "border-top-right-radius" : "5px",
-                        "overflow": "hidden", 
-                        "display": "block"
-                    };
-
-//define elements
-var memeContainer = document.createElement("div");
-var textContainer = document.createElement("div");
-var imageContainer = document.createElement("img");
-
-//removes automatic padding and margin in the body element
-var style = document.createElement("style");
-style.innerHTML = "body {padding:0; margin:0}";
-
-//build element hierarchy
-memeContainer.appendChild(style);
-memeContainer.appendChild(textContainer);
-memeContainer.appendChild(imageContainer);
-
-$(memeContainer).css(memeContainerCSS);
-$(textContainer).css(textCSS);
-$(imageContainer).css(imageCSS);
-
 /* Runs when the page finishes loading */
-$(document).ready(function() {
-    // Fires when the "Create" button is clicked
-    $('#create_button').click(onClickCreate);
+$(function() {
+    //set css for the meme itself
+    $('#meme-container').css(memeContainerCSS);
+    $('#meme-text').css(memeTextCSS);
+    $('#meme-image').css(memeImageCSS);
 
-    $( "#tabs-container" ).tabs();
+    $('#create_button').click(onClickCreate);
 });
 
 function onClickCreate() {
-    var memeTextArea = document.getElementById('meme_text');
-    var memeImageInput = document.getElementById('meme_image');
+    var memeTextArea = document.getElementById('meme-text-input');
+    var memeImageInput = document.getElementById('meme-image-input');
+    var missingWarning = document.getElementById('missing-warning');
 
     //checks if both an image and text are present
     if(memeImageInput.files[0] && memeTextArea.value) {
         var memeImage = memeImageInput.files[0];
         var memeText = memeTextArea.value;
+        missingWarning.style.display = "none";
 
         var reader = new FileReader();
         reader.onload = createMemeAfterImageIsURI(memeText, reader);
         reader.readAsDataURL(memeImage);
     }
-    //If image or text missing, alert the user
     else {
-        alert("Missing text or picture");
+        //TODO add style to warning sign
+         missingWarning.style.display = "block";
     }
 }
 
 function setMemeHTML(memeText, memeImageDataURI) {
-    textContainer.innerHTML = memeText;
+    var imageContainer = document.getElementById('meme-image');
+
+    document.getElementById('meme-text').innerHTML = memeText;
     imageContainer.src = memeImageDataURI;
 
     //make sure image doesn't go over the height and width limits.
     //also preserves image proportions.
     if (imageContainer.naturalHeight > imageContainer.naturalWidth) {
-        $(imageContainer).css({"height" : "450px", "width" : "auto", 
-            "margin-left" : "auto", "margin-right" : "auto", "padding-right" : "20px"});
+        $(imageContainer).css({"height" : "450px", "width" : "auto"});
     } else {
-        $(imageContainer).css({"width" : "500px", "height" : "auto"});
+        $(imageContainer).css({"width" : "480px", "height" : "auto"});
     }
 }
 
-function getImageDownloadContainer(imgSrc) {
-    var divisionLine = document.createElement("hr");
-    var downloadBtn = document.createElement("a");
-    var downloadGlyphicon = document.createElement("span");
-    var meme = document.createElement("img");
-
-    $(downloadGlyphicon).addClass("glyphicon glyphicon-download-alt");
-
-    downloadBtn.appendChild(downloadGlyphicon);
-    downloadBtn.innerHTML += ' Download';
-    downloadBtn.id = "download-meme";
-    downloadBtn.href = imgSrc;
-    downloadBtn.download = "meme_" + Date.now();
-
-    meme.src = imgSrc;
-
-    $(downloadBtn).addClass("btn btn-lg btn-block action-button");
-    $(meme).addClass("image");
-    $(meme).attr("id", "meme");
-
-    return [divisionLine, meme, downloadBtn];
+function createStyleSheetLink(link) {
+    var ss = document.createElement("link");
+    ss.type = "text/css";
+    ss.rel = "stylesheet";
+    ss.href = "meme_style.css";
 }
 
 function createMemeAfterImageIsURI(memeText, reader) {
     return function () {
         var memeImageDataURI = reader.result;
+        var memeContainer = document.getElementById('meme-container');
+
         setMemeHTML(memeText, memeImageDataURI);
 
-        //used to get the size of the canvas
-        memeContainer.style.visibility = "hidden";
-        document.body.appendChild(memeContainer);
-
-        var canvasWidth = $(memeContainer).outerWidth() + 2*memePadding;
+        //get the size of the canvas
+        var canvasWidth = $(memeContainer).outerWidth();
         var canvasHeight = $(memeContainer).outerHeight();
-        
-        document.body.removeChild(memeContainer);
-        memeContainer.style.visibility = "";
+        console.log(canvasWidth);
 
         //make canvas element
         var canvas = document.createElement("canvas");
@@ -136,20 +74,57 @@ function createMemeAfterImageIsURI(memeText, reader) {
         ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
         //call rasterizeHTML with html we created
-        var html = memeContainer.outerHTML;
-        rasterizeHTML.drawHTML(html, canvas).then(function success(renderResult) {
+        var canvasDocument = document.implementation.createHTMLDocument();
+        canvasDocument.body.appendChild(memeContainer.cloneNode(true));
+
+        //fixes the extra margin added by default
+        var marginFixStyle = document.createElement('style');
+        marginFixStyle.innerHTML = "body {margin: 0; padding: 0;}";
+        canvasDocument.head.appendChild(marginFixStyle);
+
+        canvasDocument.getElementById('meme-container').style.display = "block";
+
+        rasterizeHTML.drawDocument(canvasDocument, canvas).then(function success(renderResult) {
             //convert canvas to a png image and attach it to the page
             var memeSrc = canvas.toDataURL();
 
-            if($("#meme").length == 0) {
-                var downloadMemeElts = getImageDownloadContainer(memeSrc);
-                $("#main-content").append(downloadMemeElts);
-            } else {
-                $("#meme").attr("src", memeSrc);
-                $("#download-meme").attr("href", memeSrc);
+            var downloadContent = document.getElementById('download-content');
+            var downloadButton = document.getElementById('download-meme-button');
+            var downloadMemeImage = document.getElementById('meme');
+
+            downloadButton.href = memeSrc;
+            downloadButton.download = 'meme_' + Date.now();
+            downloadMemeImage.src = memeSrc;
+
+            if(downloadContent.style.display == "none") {
+                downloadContent.style.display = "block";
             }
         }, function error(e) {
                 console.log('there was an error');
         });
     };
 }
+
+var memeTextCSS = {"font-family" : "Helvetica Neue,Helvetica,Arial,sans-serif",
+                "margin": "5px 10px 0px 10px",
+                "font-size" : "26px",
+                "line-height" : "32px",
+                "font-weight" : "300", 
+                "letter-spacing" : ".01em", 
+                "word-wrap" : "break-word", 
+                "color" : "#292f33", 
+                "width" : "inherit"
+            };
+
+var memeImageCSS = {"border-radius": "5px", 
+                "margin": "10px auto",
+                "position": "relative", 
+                "display" : "block", 
+                "overflow": "hidden", 
+                "vertical-align": "top"
+            };
+
+var memeContainerCSS = {"background-color" : "#fff",
+                        "width" : "500px",
+                        "overflow": "hidden"
+                    };
